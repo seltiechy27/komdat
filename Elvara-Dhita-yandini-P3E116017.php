ef9a408cfe55864befee09212d1b885a75840529
VaraAisyah98 <39480988+VaraAisyah98@users.noreply.github.com>
2018-06-07 16:02:49 +0700

    Tugas Final Komdat

---
+++ Elvara-Dhita-yandini-P3E116017.php
@@ -0,0 +1,10 @@
+<html>
+<head> <title> elvara </title>
+</head>
+<body>
+<h1> Selamat datang para saudara ku <h1>
+<table width="80%" border=1>
+<tr>
+	<td>karir</td> <td>profil</td> <td>artikel</td>
+</table>
+</html>
