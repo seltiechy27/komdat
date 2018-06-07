63f4b01eebf4670dfe285e6f3f56e578099b6182
PutriPratiwi16 <39506462+PutriPratiwi16@users.noreply.github.com>
2018-06-07 17:14:46 +0800

    TUGAS FINAL PUTRI IKE PRATIWI P3E116022
    TUGAS FINAL

---
+++ PUTRI-IKE-PRATIWI-P3E116022.php
@@ -0,0 +1,238 @@
+
+PAW | programming-animation-website
+ABOUT
+SUB MENU
+CONTACT US
+  HTML  Kumpulan Syntax Dasar HTML
+KUMPULAN SYNTAX DASAR HTML
+ Budi Sulistyo  10:17 PM
+
+
+Halo sobat paw, tutorial kali ini akan menjelaskan tentangkumpulan syntax dasar HTML
+
+
+
+
+
+
+
+
+
+Struktur Dasar
+
+<html>
+<head>
+<!-ini adalah bagian header-->
+<title>judul web</title>
+</head>
+<body>
+<!-tempat menuliskan kode-->
+</body>
+</html>
+
+<!--…-->
+
+<!—Ini adalah komentar dan tidak akan ditampilkan dibrowser-->
+
+<a>
+Mendefinisikan tag anchor. Digunakan untuk membuat link ke dokumen lain menggunakan atribut herf dan juga untuk membuat bookmark dokumen mengunakan atribut name.
+Contoh
+<body>
+<a href=http://www.google.com”>kunjungi website google</a>
+</body>
+
+<b>
+Digunakan untuk mencetak tebal suatu teks.
+Contoh:
+<b>teks tercetak tebal (bold)</b>
+
+<body>
+Digunakan untuk mendefinisikan badan html, yang didalamnya merupakan tempat untuk meletakan elemen-elemen html lainnya, seperti gambar, teks, table, form, dll
+Contoh:
+<html>
+<head>
+<title>judul web</title>
+</head>
+<body>
+<!-ini adalah bagian badan tempat anda menuliskan kode-->
+</body>
+</html>
+
+<br>
+Digunakan untuk menyisipkan sebuah baris(ganti baris)
+Contoh:
+<body>
+Salah satu aplikasi internet adalah web<br>
+Html adalah kode dasar untuk membuat web<br>
+</body>
+
+<center>
+Digunakan untuk menampilkan teks ditengah halaman 
+Contoh:
+<center> teks ini akan tampil ditengah halaman</center>
+
+<div>
+Digunakan untuk mendifinisikan bagian dari sebuah halaman web. Seringkali digunakan untuk mengelompokan elemen-elemen HTML dengan format style tertentu.
+Contoh:
+<div style=’color:#ff0000”>
+<h3>ini merupakan judul</h3>
+<p>ini merupakan paragraph</p>
+</div>
+
+<form>
+Digunakan untuk mendifinisikan sebuah form masukan dari pengguna
+Contoh:
+<form action=”action.php” method=”get”>
+Nama:  <input type=”text” name=”f_nama”/>
+<br>
+email:  <input type=”text” name=”f_email”/>
+<br>
+<input type=”submite” value=’simpan”/>
+</form>
+
+<h1> sampai dengan <h6>
+Digunakan untuk mendinisikan bagian atas (heading) teks. Dimulai dari ukuran besar<h1> hingga ukuran kecil<h6>
+Contoh:
+<h1> HTML adalah kode dasar membuat web</h1>
+<h2> HTML adalah kode dasar membuat web</h2>
+<h3> HTML adalah kode dasar membuat web</h3>
+<h4> HTML adalah kode dasar membuat web</h4>
+<h5> HTML adalah kode dasar membuat web</h5>
+<h6> HTML adalah kode dasar membuat web</h6>
+
+<head>
+Digunakan untuk mendifinisikan bagian atas(head) sebuah dokumen html. Bagian ini biasanya digunakan untuk mendifinisikan tag <base>, <link>, <meta>, <script>, <style>, dan <title>.
+Contoh:
+<head>
+<title>judul web</title>
+</head>
+
+<hr>
+Digunakan untuk mendifinisikan sebuah garis horizontal dalam halaman html
+Contoh:
+Salah satu aplikasi internet adalah web<hr>
+Html adalah kode dasar untuk membuat web
+
+<i>
+Digunakan untuk mencetak miring suatu teks.
+Contoh:
+<i>teks tercetak miring(italic)</i>
+
+<p>
+Digunakan untuk membuat paragraph baru.
+
+<select>
+Digunakan untuk mendinisikan daftar pilihan
+Contoh:
+<select>
+<option value=”detik”>detik</option>
+<option value=”viva”>vivanews</option>
+<option value=”glodokshop”>glodok shop</option>
+<option value=”bhineka”>Bhineka</option>
+</select>
+
+<Strong>
+Digunakan untuk mempertegas tampilan suatu teks.
+Contoh:
+Kode dasar untuk membuat web adalah <strong>HTML</strong>
+
+<Style>
+Digunakan untuk mendifinisikan informasi style pada dokumen html
+Contoh:
+<head>
+<style type=”text/css”>
+Span.blue {color:lightskyblue;
+Font-weight:bold}
+</style>
+</head>
+<body>
+Kode dasr untuk membuat web adalah <span class=”blue”>HTML</span>
+</body>
+
+<table>
+Digunakan untuk membuat table dalam halaman web.
+Contoh:
+<table border=”1”>
+<caption>daftar situs</caption><tr>
+<th>berita</th>
+
+<title>
+Digunakan untuk mendifinisikan judul pada dokumen html.
+Contoh:
+<head>
+<title>judul web</title>
+</head>
+
+<u>
+Digunakan untuk menambahkan garis bawah suatu teks
+Contoh:
+<u>teks bergaris bawah(bold)</u>
+
+<ul>
+Digunakan untuk membuat daftar yang tak berurut(unordered list) menggunakan symbol bullets.
+Contoh:
+<ul>
+<li>webpribadi</li>
+<li>blog</li>
+<li>facebook</li>
+</ul>
+
+Tweet
+Share
+Share
+Share
+Share
+RELATED POST
+NEXT
+Dasar MySQL 1
+PREVIOUS
+Penjelasan, Fungsi dan Cara Instalasi XAMPP
+3 COMMENTS CLICK HERE FOR COMMENTS
+Nur Aisyah May 22, 2017 at 5:57 AM
+No more live link in this page
+
+Reply
+avatar
+Tokekbudug12 February 1, 2018 at 7:42 AM
+TQ
+
+Reply
+avatar
+Ujang sanjaya April 8, 2018 at 9:27 AM
+Terimakasi contoh2 ny gan
+
+
+Reply
+avatar
+Conversion Emoticon
+
+ 
+POPULARCATEGORIESARSIP
+ Penjelasan, Fungsi dan Cara Instalasi XAMPP
+Penjelasan, Fungsi dan Cara Instalasi XAMPP
+ Membuat Databaset di SQL Server Management Studio 2008
+Membuat Databaset di SQL Server Management Studio 2008
+ Kumpulan Syntax Dasar HTML
+Kumpulan Syntax Dasar HTML
+LIKE ON FACEBOOK
+
+STATISTIC
+
+  free html hit counter
+GAME SUDOKU
+
+LABEL
+HTML
+Java
+PHP-MySQL
+SQL Server
+VB 6
+VB.Net
+AUTHOR
+Paw
+
+Budi Sulistyo
+
+Vinita Chandani
+Copyright © 2014 PAW | programming-animation-website All Right Reserved
+Design by Arlina Powered by Blogger
